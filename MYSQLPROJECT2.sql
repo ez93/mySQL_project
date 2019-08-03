832695423c5d4ac035de152f2373a7f6007377cd
ez93 <46770373+ez93@users.noreply.github.com>
2019-08-03 16:23:41 -0700

    Add files via upload

---
+++ MYSQLPROJECT2.sql
@@ -0,0 +1,51 @@
+USE university_schema;
+
+-- most courses in common
+SELECT *
+FROM professors p
+RIGHT JOIN students s
+ON p.professors_id=students_user_id
+ORDER BY students_enrolled DESC;
+
+-- GROUP students by the courses they are enrolled in 
+SELECT *
+FROM courses c
+RIGHT JOIN students s
+ON c.courses_id=students_user_id
+ORDER BY students_enrolled DESC;
+
+-- TOP grades by each student
+SELECT *
+FROM students s
+JOIN grades g
+ON s.students_user_id= g.grades_id
+ORDER BY grades ASC;
+
+
+-- AVG grade given out
+SELECT *
+FROM courses c
+JOIN professors p
+ON c.courses_id=p.professors_id
+JOIN grades g
+ON g.grades_id=p.professors_id;
+
+-- Summary report with most challenging course 
+SELECT * 
+FROM professors p
+JOIN courses c
+ON p.professors_students_user_id = c.courses_students_user_id
+JOIN students s
+ON s.students_user_id =c.courses_students_user_id
+JOIN grades g
+ON g.grades_students_user_id= c.courses_students_user_id
+ORDER BY grades DESC;
+
+
+
+
+
+
+
+
+
